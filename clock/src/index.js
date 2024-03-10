7eae837cabd2443b35750b79fa882baff739e74a
haribo09876 <haribo09876@gmail.com>
2024-03-10 22:04:09 +0900

    20240310
    by KYW

--- clock/src/index.js
+++ clock/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Clock from "./Clock/Clock.jsx";
+import Clock from "./Clock.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- coin-tracker/src/App.js
+++ coin-tracker/src/App.js
@@ -1,37 +1,3 @@
-import { useState } from "react";
-
-function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
-  return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          type="text"
-          onChange={onChange}
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+function App() {}
 
 export default App;
